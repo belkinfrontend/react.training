cff702f41a42352c3a723dd0a168b6d3990fb51f
Dmitry Belkin <dmitry.belkin@dataart.com>
2019-02-15 19:56:27 +0200

    prettier and eslint were added

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import logger from 'redux-logger';
+
+const inititalState = {};
+
+const store = createStore(
+  rootReducer,
+  inititalState,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
